233ec7854fcad1689ebc1259602f3876aaaaa120
AnujAlopa <averma@amalgamrx.com>
2024-03-26 08:36:16 +0530

    pushed server

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-const express = require('express');
+const envVariable = require("./config/envValues.js");
+const routerConfig = require("./router.js");
+const express = require("express");
+const models = require("./models/index");
 const app = express();
-const port = 4000;
+const cors = require("cors");
+const DB = require("./utils/server.js").sequelizedb;
+const server = require("./utils/server.js")
 const path = require("path");
 
-let http = require("http");
-let server = http.createServer(app);
 
+DB.initialize(envVariable);
+for (let prop of Object.keys(DB)) {
+  if (prop !== "initialize") {
+    DB[prop] = models.attachModels(DB[prop].sequelize, DB[prop].Sequelize);
+  }
+}
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
-});
+function getDBInstance() {
+  let country = envVariable.defaultCountry;
+  return DB[country];
+}
+server.setupApp(app, getDBInstance)
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({limit: '25mb', extended: true}));
+app.use(cors());
+
+app.use("/api", routerConfig());
 
 const _dirname = path.dirname("")
 const buildPath = path.join(_dirname  , "./my-app/build");
 app.use(express.static(buildPath))
 
 
-app.get("/", function(req, res){
+app.get("/*", function(req, res){
   res.sendFile(
       path.join(__dirname, "./my-app/build/index.html"),
       function (err) {
@@ -28,15 +44,17 @@ app.get("/", function(req, res){
 
 })
 
-server.listen(4001, function (err) {
-   console.log(`Example app listening at http://localhost:${4001}`);
+Object.defineProperty(DB, "usingSequelize", {
+  value: true,
+  enumerable: false,
+});
 
-  if (err) {
-    logger.error(err);
-    throw err;
-  }
-  });
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
+server.heartbeat.initialise(
+  DB,
+  envVariable.healthCheckPort
+);
+server.startServer(app).catch((err) => {
+    console.log(err)
+//   logger.info(err);
+});
 
